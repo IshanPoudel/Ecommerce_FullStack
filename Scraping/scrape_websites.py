d5640a339203346d7e99e61a54bd4e49a0648e3c
IshanPoudel <ishan.poudel@mavs.uta.edu>
2022-09-24 20:59:28 -0500

    Added scafoolding

--- Scraping/scrape_websites.py
+++ Scraping/scrape_websites.py
@@ -1,1 +1,17 @@
 #Use bs4 and Selenium to scrape data prices and have them store it in a mongoDB instance. 
+
+from bs4 import BeautifulSoup
+
+
+chrome_options = Options()  # Instantiate an options class for the selenium webdriver
+# chrome_options.add_argument("--headless")  # So that a chrome window does not pop up
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+time.sleep(10)
+
+url = "https://www.amazon.com/s?k=kindle"
+
+driver.get(url)
+time.sleep(20)
+
+html_text = BeautifulSoup(html_text , 'htmll.parser')
+print(html_text)
