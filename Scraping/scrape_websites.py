cebf58b5e991f587fe41fee5b4c6c4989660639b
IshanPoudel <ishan.poudel@mavs.uta.edu>
2022-10-17 19:51:47 -0500

    Added docker changes

--- Scraping/scrape_websites.py
+++ Scraping/scrape_websites.py
@@ -1,7 +1,7 @@
 #Use bs4 and Selenium to scrape data prices and have them store it in a mongoDB instance. 
 
 from bs4 import BeautifulSoup
-from Selenium import Options
+from selenium import options
 # import websites.json
 
 
